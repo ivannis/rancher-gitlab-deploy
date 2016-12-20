6834233ecb2fed1cf28f29c329f143ba7661f2b6
Ivan <ivannis.suarez@gmail.com>
2016-12-20 16:47:53 +0100

    added sidekicks upgrade support

--- rancher_gitlab_deploy/cli.py
+++ rancher_gitlab_deploy/cli.py
@@ -152,6 +152,9 @@ def main(rancher_url, rancher_key, rancher_secret, environment, stack, service, 
     # copy over the existing config
     upgrade['inServiceStrategy']['launchConfig'] = service['launchConfig']
 
+    for secondaryLaunchConfig in service['secondaryLaunchConfigs']:
+        upgrade['inServiceStrategy']['secondaryLaunchConfigs'].append(secondaryLaunchConfig.copy())
+
     if new_image:
         # place new image into config
         upgrade['inServiceStrategy']['launchConfig']['imageUuid'] = 'docker:%s' % new_image
